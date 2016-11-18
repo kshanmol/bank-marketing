ab8b320921e284533d63149f1306eb5f4738e869
Satyam Kumar <satyam14096@iiitd.ac.in>
2016-11-18 23:59:35 +0530

    More statistics, learning curves, etc

--- gaussian_svm.py
+++ gaussian_svm.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.cross_validation import KFold
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,7 +17,7 @@ import get_data
 
 CONST_RANDOM_SEED = 42
 
-def FitModel(train_data, train_labels, cross_val_splits = 5, random_seed = CONST_RANDOM_SEED, max_iter = 1000):
+def FitModel(train_data, train_labels, n_folds = 5, random_seed = CONST_RANDOM_SEED, max_iter = 1000):
 
 	random.seed(random_seed)
 
@@ -41,7 +42,7 @@ def FitModel(train_data, train_labels, cross_val_splits = 5, random_seed = CONST
 	# 		'dual': [False], 'random_state': [random_seed], 'loss': ['squared_hinge']}
 	# ]
 
-	# best_classifier = GridSearchCV(estimator = SVC(), param_grid = param_grid, cv = cross_val_splits)
+	# best_classifier = GridSearchCV(estimator = SVC(), param_grid = param_grid, cv = n_folds)
 	# best_classifier.fit(train_data, train_labels)
 
 	# print best_classifier.cv_results_['param_gamma']
@@ -91,6 +92,52 @@ def roc_statistics(test_data, test_labels, scaler, model):
 	plt.legend(loc = "lower right")
 	plt.show()
 
+
+def plot_learning_curves(data, labels, n_folds = 5, random_seed = CONST_RANDOM_SEED):
+	data_perc, train_scores, cv_scores = [], [], []
+
+	for train_data_perc in range(10, 110, 10):
+		cv_score, train_score = 0, 0
+		train_data = data[: (len(data) * train_data_perc) / 100]
+		train_labels = labels[: (len(labels) * train_data_perc) / 100]
+
+		scaler = StandardScaler(); scaler.fit(train_data)
+		train_data = scaler.transform(train_data)
+
+		kf = KFold(len(train_data), n_folds = n_folds, shuffle = True, random_state = random_seed)
+
+		train_data, train_labels = np.asarray(train_data), np.asarray(train_labels)
+		for train_index, test_index in kf:
+			X_train, X_test = train_data[train_index], train_data[test_index]
+			y_train, y_test = train_labels[train_index], train_labels[test_index]
+
+			opt_hyperparameters = {'kernel': 'rbf', 'C': 32, 'gamma': 0.0009765625}
+
+			classifier = SVC(**opt_hyperparameters)
+			classifier.fit(X_train, y_train)
+
+			train_score += classifier.score(X_train, y_train)
+			cv_score += classifier.score(X_test, y_test)
+
+		cv_score /= n_folds
+		train_score /= n_folds
+		print cv_score, train_score
+		data_perc.append(train_data_perc)
+		cv_scores.append(cv_score)
+		train_scores.append(train_score)
+
+	cv_scores = map(lambda x: 1 - x, cv_scores)
+	train_scores = map(lambda x: 1 - x, train_scores)
+	plt.figure()
+	plt.plot(data_perc, cv_scores, label = "CV-Error")
+	plt.plot(data_perc, train_scores, label = "Training Error")
+	plt.ylim([0.0, 0.2])
+	plt.xlabel('Training Data Percentage')
+	plt.ylabel('Classification Error')
+	plt.title('Learning Curve')
+	plt.legend(loc = 'lower right')
+	plt.show()
+
 if __name__ == '__main__':
 
 	random.seed(CONST_RANDOM_SEED)
@@ -101,10 +148,13 @@ if __name__ == '__main__':
 
 	train_data, test_data, train_labels, test_labels = train_test_split(x, y, test_size = 0.33, random_state = CONST_RANDOM_SEED)
 
-	# print len(train_data[0]), len(test_data[0])
-	scaler, classifier = FitModel(train_data, train_labels)
+	# # print len(train_data[0]), len(test_data[0])
+	# scaler, classifier = FitModel(train_data, train_labels)
 
-	# print classifier
-	TestModel(test_data, test_labels, scaler, classifier)
+	# # print classifier
+	# TestModel(test_data, test_labels, scaler, classifier)
 
-	roc_statistics(test_data, test_labels, scaler, classifier)
+	# roc_statistics(test_data, test_labels, scaler, classifier)
+
+
+	plot_learning_curves(train_data, train_labels)
